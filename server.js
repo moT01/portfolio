b368e478cfcd8a5173d500687908693df53de675
moT01 <tmondloch01@gmail.com>
2020-03-22 13:16:35 -0500

    deplay test

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 // serve static assets in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static('client/public'))
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
